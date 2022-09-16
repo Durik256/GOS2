72bd594b787b60759647d1615b411866339d0546
Durik256 <61597745+Durik256@users.noreply.github.com>
2022-09-16 03:18:45 +0300

    C# source code *.TX3 to *.TXT

---
+++ TX3toTXT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace TX3toTXT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                byte[] Buffer = File.ReadAllBytes(args[0]);
+
+                for (int x = 0; x < Buffer.Length; x++)
+                {
+                    if (Buffer[x] >= 0xF)
+                        Buffer[x] = (byte)(14 - Buffer[x]);
+                }
+                File.WriteAllBytes(args[0].Replace(".tx3", ".txt").Replace(".TX3", ".txt"), Buffer);
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
