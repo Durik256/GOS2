f7b2cc8b2d2cd8986bbdc0a2cf2c67e6b56a8b51
Durik256 <61597745+Durik256@users.noreply.github.com>
2022-09-17 00:54:45 +0300

    C# source code *.MPI to *.WAV and *.WAV to *.MPI

---
+++ MPI_WAV.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace TX3toTXT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                byte[] wavHeader = { 82, 73, 70, 70, 151, 121, 2, 0, 87, 65, 86, 69 };
+                byte[] mpiHeader = { 70, 79, 80, 73, 151, 121, 2, 0, 71, 69, 80, 79 };
+                byte[] Buffer = File.ReadAllBytes(args[0]);
+
+                string extension = Path.GetExtension(args[0]);
+
+                if (extension.ToLower() == ".mpi")
+                {
+                    for (int x = 0; x < wavHeader.Length; x++)
+                        Buffer[x] = wavHeader[x];
+                    
+                    extension = ".wav";
+                }
+                else if (extension.ToLower() == ".wav")
+                {
+                    for (int x = 0; x < mpiHeader.Length; x++)
+                        Buffer[x] = mpiHeader[x];
+
+                    extension = ".mpi";
+                }
+                else
+                    Environment.Exit(0);
+
+                File.WriteAllBytes(Path.ChangeExtension(args[0], extension), Buffer);
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
