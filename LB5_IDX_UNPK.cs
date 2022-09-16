28ebf0f68cbd71a50b61c7b794b9932f61b70acc
Durik256 <61597745+Durik256@users.noreply.github.com>
2022-09-16 22:50:14 +0300

    C# source code LB5 and IDX unpack

---
+++ LB5_IDX_UNPK.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TX3toTXT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string extension = Path.GetExtension(args[0]);
+                string lbFile = "";
+                string idxFile = "";
+                
+                if (extension.ToLower() == ".lb5")
+                {
+                    lbFile = args[0];
+                    idxFile = Path.ChangeExtension(args[0], ".idx");
+                }  
+                else if (extension.ToLower() == ".idx")
+                {
+                    idxFile = args[0];
+                    lbFile = Path.ChangeExtension(args[0], ".lb5");
+                }
+                else
+                    Environment.Exit(0);
+
+                using (FileStream fsIDX = new FileStream(idxFile, FileMode.Open))
+                {
+                    using (BinaryReader rIDX = new BinaryReader(fsIDX))
+                    {
+                        int numFile = rIDX.ReadInt32();
+                        using (FileStream fsLB = new FileStream(lbFile, FileMode.Open))
+                        {
+                            using (BinaryReader rLB = new BinaryReader(fsLB))
+                            {
+                                for (int i = 0; i < numFile; i++)
+                                {
+                                    int offset = rIDX.ReadInt32();
+                                    int size = rIDX.ReadInt32();
+
+                                    string name = new string(rIDX.ReadChars(16), 1, 15).Replace("\0", string.Empty);
+
+                                    if (Path.GetExtension(name).ToLower() == ".bmp")
+                                        name = Path.ChangeExtension(name, ".bp3");
+
+                                    if (Path.GetExtension(name).ToLower() == ".txt")
+                                        name = Path.ChangeExtension(name, ".tx3");
+
+                                    Console.WriteLine(name + " offset:" + offset.ToString() + " size:" + size.ToString());
+
+                                    fsLB.Seek(offset, SeekOrigin.Begin);
+                                    byte[] Buffer = rLB.ReadBytes(size);
+                                    File.WriteAllBytes(name, Buffer);
+                                }
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
